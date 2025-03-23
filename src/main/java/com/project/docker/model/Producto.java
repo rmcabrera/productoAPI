42d1495bc5a213e517832b8c0aa46d902a6bb0b0
rmcabrera <mateussearc@gmail.com>
2025-03-23 13:13:04 -0500

    version con H2 modo server

--- src/main/java/com/project/docker/model/Producto.java
+++ src/main/java/com/project/docker/model/Producto.java
@@ -1,5 +1,6 @@
 package com.project.docker.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,11 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "producto")
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private String descripcion;
--- src/main/java/com/project/docker/service/ProductoServiceImpl.java
+++ src/main/java/com/project/docker/service/ProductoServiceImpl.java
@@ -1,48 +1,53 @@
 package com.project.docker.service;
 
 import com.project.docker.model.Producto;
+import com.project.docker.repository.ProductoRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ProductoServiceImpl implements ProductoService{
 
-    private final List<Producto> productos = new ArrayList<>();
-    private Long contadorId = 1L;
+    private final ProductoRepository productoRepository;
+
+    public ProductoServiceImpl(ProductoRepository productoRepository) {
+        this.productoRepository = productoRepository;
+    }
 
     @Override
     public List<Producto> listar() {
-        return productos;
+        return productoRepository.findAll();
     }
 
     @Override
     public Optional<Producto> obtenerPorId(Long id) {
-        return productos.stream().filter(p -> p.getId().equals(id)).findFirst();
+        return productoRepository.findById(id);
     }
 
     @Override
     public Producto agregar(Producto producto) {
-        producto.setId(contadorId++);
-        productos.add(producto);
-        return producto;
+        return productoRepository.save(producto);
     }
 
     @Override
     public Optional<Producto> actualizar(Long id, Producto nuevoProducto) {
-        return obtenerPorId(id).map(producto -> {
+        return productoRepository.findById(id).map(producto -> {
             producto.setNombre(nuevoProducto.getNombre());
             producto.setDescripcion(nuevoProducto.getDescripcion());
             producto.setPrecio(nuevoProducto.getPrecio());
-            return producto;
+            return productoRepository.save(producto);
         });
     }
 
     @Override
     public boolean eliminar(Long id) {
-        return productos.removeIf(p -> p.getId().equals(id));
+        if (productoRepository.existsById(id)) {
+            productoRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 
 }
